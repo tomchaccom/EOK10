638fd7acc19b1c72bcef0cd9b089d6483ccc9b37
tomchccom <dreamkms2014@gmail.com>
2026-01-29 19:20:03 +0900

    feat : add data type

---
+++ packages/core/types/index.ts
@@ -0,0 +1,21 @@
+export type AttentionState = number;
+
+export type FeasibilityResult = "OK" | "RISK" | "OVERLOAD";
+
+export type OffloadDecision = "HUMAN" | "AGENT";
+
+export interface Action {
+  id: string;
+  label: string;
+  description?: string;
+}
+
+export interface Task {
+  id: string;
+  title: string;
+  description?: string;
+  attention: AttentionState;
+  feasibility: FeasibilityResult;
+  offloadDecision: OffloadDecision;
+  actions?: Action[];
+}
